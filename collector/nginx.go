86fa2b5f14b0b320f60b3c803683e086c4fa5a37
heungbot <cjg02011@gmail.com>
2025-07-11 22:30:45 +0900

    ADD: Upstream Parse & TCP Check

--- collector/nginx.go
+++ collector/nginx.go
@@ -1,7 +1,6 @@
 package collector
 
 import (
-	"fmt"
 	"io/fs"
 	"log/slog"
 	"os"
@@ -22,8 +21,9 @@ type NginxCollector struct {
 	mutex       sync.Mutex
 
 	// Custom For Nginx Proxy //
-	nginxConfigPath string
-	configModDesc   *prometheus.Desc
+	nginxConfigPath         string
+	configModDesc           *prometheus.Desc
+	upstreamHealthCheckDesc *prometheus.Desc
 }
 
 // NewNginxCollector creates an NginxCollector.
@@ -46,6 +46,11 @@ func NewNginxCollector(nginxClient *client.NginxClient, namespace string, constL
 			"NGINX config 파일별 마지막 수정 시각(Unix timestamp)",
 			[]string{"file"}, constLabels,
 		),
+		upstreamHealthCheckDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "upstream", "health_check_status"),
+			"Proxy Target의 TCP 연결 상태(1: 성공, 0: 실패)",
+			[]string{"file", "target"}, constLabels,
+		),
 		nginxConfigPath: nginxConfigPath,
 	}
 }
@@ -60,6 +65,7 @@ func (c *NginxCollector) Describe(ch chan<- *prometheus.Desc) {
 	}
 
 	ch <- c.configModDesc
+	ch <- c.upstreamHealthCheckDesc
 }
 
 // Collect fetches metrics from NGINX and sends them to the provided channel.
@@ -96,7 +102,7 @@ func (c *NginxCollector) Collect(ch chan<- prometheus.Metric) {
 	////// CUSTOM FOR NGINX PROXY //////
 	files := []string{c.nginxConfigPath}                                 // []string{"/home1/irteam/apps/nginx/nginx.conf"}
 	confdDir := filepath.Join(filepath.Dir(c.nginxConfigPath), "conf.d") // "/home1/irteam/apps/nginx/conf.d"
-	// 순회 하면서 files slice 추가.
+	// 순회 하면서 files slice에 추가.
 	_ = filepath.WalkDir(confdDir, func(path string, dir fs.DirEntry, err error) error {
 		if err == nil && !dir.IsDir() {
 			files = append(files, path)
@@ -104,14 +110,34 @@ func (c *NginxCollector) Collect(ch chan<- prometheus.Metric) {
 		return nil
 	})
 
-	c.logger.Info(fmt.Sprintf("FILES: %v", files))
-
 	for _, f := range files {
 		info, err := os.Stat(f)
 		if err != nil || !strings.HasSuffix(info.Name(), ".conf") {
 			c.logger.Warn("skip config file", "file", f, "err", err)
 			continue
 		}
+
+		proxyTargets, err := extractProxyTarget(f)
+		if err != nil {
+			c.logger.Warn("error extracting proxy targets", "file", f, "error", err.Error())
+			continue
+		}
+
+		// prox target 추출 후, tcp 연결 테스트 수행
+		for _, target := range proxyTargets {
+			netResult, err := tcpTest(target)
+			if err != nil {
+				c.logger.Warn("error testing proxy target", "file", f, "target", target, "error", err.Error())
+			}
+			ch <- prometheus.MustNewConstMetric(
+				c.upstreamHealthCheckDesc,
+				prometheus.GaugeValue,
+				netResult,
+				f, target,
+			)
+		}
+
+		// 파일의 마지막 수정 시각을 Unix timestamp로 치환하여 메트릭으로 전송
 		ch <- prometheus.MustNewConstMetric(
 			c.configModDesc,
 			prometheus.GaugeValue,
